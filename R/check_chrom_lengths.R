07e46b05dcfbab256720afab89d372e12c2be9aa
Giglio <12001003180080@FEDIDCARD.GOV>
2021-10-29 13:34:34 -0600

    Added documentation check_chrom_lengths

--- R/check_chrom_lengths.R
+++ R/check_chrom_lengths.R
@@ -1,11 +1,24 @@
-############ function for checking chromosome lengths ###################
-###### Compares the last position on each chromosome (found in meta data file)
-###### to the chromosome length (found in recombination map chrom_len)
-
 #' check that the chromosome lengths are acceptable given recombination rates
 #'
-#' Needs more documenting.
+#' For gscramble to work properly, all variant positions on a chromosome (found in the meta data file)
+#' must be equal to or less than the total chromosome length found in the recombination map. In other words,
+#' the variant positions must be within the the total length of each chromosome. The check_chrom_lengths()
+#' function checks that variant positions on each chromosome do not exceed the total chromosome length from the
+#' recombination map. Input files for the function include 1) the meta data file which contains 3 columns (the chromosome ID
+#' as a character, the position of the variant as a numeric, and the name of the variant as a character) and
+#' 2) the recombination map which contains 5 columns (the chromosome ID as a character, the total length of the chromosome
+#' as a numeric, the starting position of the recombination bin as a numeric, the last position of the recombination bin as
+#' a numeric, and the recombination probability for the given bin as a numeric).
+#'
+#' @param meta a tibble with meta data for the genotype data. It must consist of the columns (chrom, pos, variant_id).
+#' @param rec a tibble with recombination map information for your study species.It must contain 5 columns (chrom, chrom_len, start_pos, end_pos, rec_prob).
+#' @return This function will return a message for each chromosome stating whether the chromosome lengths are accurate
 #' @export
+#' @examples
+#' #use built in datasets M_meta and RecRates to use the check_chrom_lengths() function
+#'
+#' check_chrom_lengths(M_meta,RecRates)
+#'
 check_chrom_lengths<-function(meta,rec){
   Flag<-FALSE
   chrom_list<-unique(meta$chrom)
