c34255ed00cf93efe5b25de402f312f451458884
Giglio <12001003180080@FEDIDCARD.GOV>
2022-09-19 16:30:39 -0600

    Update documentation for data objects.

--- R/data.R
+++ R/data.R
@@ -5,41 +5,49 @@
 #' This dataset represents 3 distinct populations of feral swine in the United States.
 #' To make this dataset computationally efficient, only 3 chromosomes (12, 17, 18) from the pig genome were used.
 #' Further, loci were reduced to the 100 most informative loci for distinguishing the 3 populations.
-#' The genotype matrix is in a standard "two-column"
-#' format genotype data matrix for a diploid species
+#' Each row of the genotype matrix includes the genotypes for a single individual.
+#' The genotype matrix is in a standard "two-column" format for a diploid species
 #' where two adjacent columns make up a locus and each column of a locus contains an allele.
 #' Genotype data is stored in the character class.
 #' Missing data is represented by NAs.
-#' Individual IDs and population assignment can be found in \code{\link{I_meta}}
+#' Individual IDs and population assignment information can be found in \code{\link{I_meta}}
 #' Locus and chromosome information for genotypes can be found in \code{\link{M_meta}}
 #'
-#' Need to document it a little more....
-#' @format A character matrix.Each row represents an individual and
-#' @source Must add...
+#' @format A character matrix. Each row represents an individual and columns contain the genotypes for individuals
+#' in a "two-column" format where two adjacent columns make up a locus with an allele in each column.
+#' @source USDA-APHIS-WS-National Wildlife Research Center Feral Swine Genetic Archive
 #' @docType data
 #' @name Geno
 NULL
 
 
-#' Meta data for 329 individuals
+#' Metadata for 78 individuals
 #'
-#' Individuals IDs and group specifications for 329 individuals
-#' whose genotypes are in \code{\link{Geno}}
+#' The tibble 'I_meta' contains the Individuals IDs and group specifications for 78 individuals. Each row of 'I_meta'
+#' contains the metadata for an individual. 'I_meta' has two columns, a 'group' column with the group or population assignment
+#' for a given individual and an 'indiv' column with the individual sample IDs. The information in each column are stored as characters.
+#' The order of the individual IDs corresponds to the genotypes found in \code{\link{Geno}}.
 #'
-#' @format A tibble with three columns: chrom, pos, and variant_id.
-#' @source Must add...
+#' @format A tibble with two columns: group and indiv.
+#' @source USDA-APHIS-WS-National Wildlife Research Center Feral Swine Genetic Archive
 #' @docType data
 #' @name I_meta
 NULL
 
 
-#' Meta data for 25165 molecular markers
+#' Metadata for 100 molecular markers
 #'
-#' Chromosome, position, and ID for 25156 molecular
-#' markers in \code{\link{Geno}}
+#' This data set contains the metadata for the 100 most divergent SNP loci for three feral swine populations
+#' sampled in the United States. To make the dataset more computationally efficient, only 3 chromosomes were
+#' used (12,17, and 18). The metadata for the SNP loci is in a tibble with three columns:
+#' chrom (character), pos (numeric), variant_id (character).
+#' The column 'chrom' contains the chromosome ID where the SNP is located, the column 'pos' gives the base pair
+#' location on the chromosome, and the column 'variant_id' gives the name of the SNP.
+#' Each row of the metadata tibble contains the metadata for a given SNP locus.
+#' The individual genotypes for each of these SNP loci can be found in \code{\link{Geno}}.
 #'
-#' @format A tibble with two columns: group and indiv.
-#' @source Must add...
+#' @format A tibble with three columns: chrom, pos, and variant_id.
+#' @source Chromosome, position, and variant IDs are from the Sus scrofa 10.2 genome \url{https://www.ncbi.nlm.nih.gov/assembly/GCF_000003025.5/}.
 #' @docType data
 #' @name M_meta
 NULL
