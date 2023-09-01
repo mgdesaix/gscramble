d252501eb9f0b3b1cdeb2aaa7c3f67c833c37d29
mgdesaix <mgdesaix@gmail.com>
2023-08-31 20:36:30 -0600

    change formatting check accordingly

--- R/check_rec_rates_formatting.R
+++ R/check_rec_rates_formatting.R
@@ -8,11 +8,11 @@ check_rec_rates_formatting <- function(rr) {
 
   Flag <- FALSE
   msg <- ""
-  # check that max(end_pos) chrom_len+1
+  # check that chrom_len = max(end_pos) +1
   cl_tib <- rr %>%
     group_by(chrom, chrom_len) %>%
     summarise(max_end_pos = max(end_pos), .groups = "drop") %>%
-    filter(!(chrom_len == max_end_pos - 1))
+    filter(!(chrom_len == max_end_pos + 1))
 
   # check that the next start position is one more than the last
   # ending position.
@@ -31,7 +31,7 @@ check_rec_rates_formatting <- function(rr) {
 
   if(nrow(cl_tib) > 0) {
     stringy <- paste("\t- Chrom:", cl_tib$chrom, "\n")
-    err <- paste("* end_pos of right-most bin not 1 greater than chromosome length for chromosomes: \n",
+    err <- paste("* end_pos of right-most bin not 1 less than chromosome length for chromosomes: \n",
                  paste(stringy, collapse = ""))
     msg <- paste(msg, err, sep = "")
     Flag <- TRUE
